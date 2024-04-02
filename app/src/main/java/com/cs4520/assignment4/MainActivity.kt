7c0cdfbb1da173811d5c3389d3f2e17155a7f88e
patrickyu117 <patrickyu117@gmail.com>
2024-04-02 16:25:59 -0400

    Created login UI

--- app/src/main/java/com/cs4520/assignment4/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment4/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.cs4520.assignment4
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavController
@@ -17,7 +37,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.fragment.NavHostFragment
-import com.cs4520.assignment5.ProductListScreen
 import com.cs4520.assignment5.R
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -31,12 +50,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         // TODO: Do we need view model in MainActivity?
-        val vm: ProductViewModel by viewModel()
+       // val vm: ProductViewModel by viewModel()
 
         // Using ProcessLifecycleOwner to observe app lifecycle changes
         ProcessLifecycleOwner.get().lifecycle.addObserver(LifecycleEventObserver { _, event ->
             if (event == Lifecycle.Event.ON_START) {
-                viewModel.fetchLatestProductsImmediately()
+                //viewModel.fetchLatestProductsImmediately()
             }
         })
     }
@@ -79,6 +98,75 @@ fun LoginScreen(navController: NavHostController) {
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // App title text
+        Text(text = "$" + "neak",
+            fontFamily = FontFamily.Serif,
+            fontSize = 30.sp, // adjust the size
+            modifier = Modifier.padding(bottom = 85.dp) // add space at the bottom
+        )
+
+        // Username TextField
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Text)
+        )
+
+        // Password TextField
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+        )
+
+        // Add a spacer after the login input boxes
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Login Button
+        Button(
+            onClick = {
+            }
+        ) {
+            Text("Login")
+        }
+
+        // Add a spacer after the login button
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // Forgot Password Button
+        Button(
+            onClick = {
+            },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.LightGray,
+                contentColor = Color.Black
+            )
+        ) {
+            Text("Forgot Password?")
+        }
+
+        // Spacer to create additional space
+        Spacer(modifier = Modifier.height(350.dp))
+
+        // Create Account Button
+        Button(
+            onClick = {
+            },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = Color.LightGray,
+                contentColor = Color.Black
+            )
+        ) {
+            Text("Create Account")
+        }
+    }
 }
 
 @Composable
@@ -104,4 +192,10 @@ fun CheckoutScreen(navController: NavHostController) {
 @Composable
 fun ThanksScreen(navController: NavHostController) {
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginScreen(rememberNavController())
 }
