56765c0af998bafccda165a17a0fc3cad1bc3a47
bham11 <the.hampster05@gmail.com>
2024-04-10 10:56:51 -0400

    (data): edited userRepo and api to only pull from api, not database

--- app/src/main/java/com/cs4520/sneak/data/ApiService.kt
+++ app/src/main/java/com/cs4520/sneak/data/ApiService.kt
@@ -4,7 +4,10 @@ import com.cs4520.sneak.data.database.Shoe
 import com.cs4520.sneak.data.database.User
 
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -12,6 +15,13 @@ interface ApiService {
 
     @GET(SneakApi.USERS)
     suspend fun getUsers(): Response<List<User>>
+
+    @GET(SneakApi.USERS + "/{userName}")
+    suspend fun getUser(@Path(value="userName")userName: String): Response<User>
+
+    @POST(SneakApi.USERS + "/{userName}")
+    suspend fun editUser(@Path(value="userName")userName: String,
+                         @Body body: Map<String, String>): Response<Any>
 
     @GET(SneakApi.SHOES)
     suspend fun getShoes(): Response<List<Shoe>>
--- app/src/main/java/com/cs4520/sneak/data/UserRepository.kt
+++ app/src/main/java/com/cs4520/sneak/data/UserRepository.kt
@@ -29,6 +29,37 @@ class UserRepository(val userDao: UserDao) {
         }
     }
 
+    suspend fun getUser(userName: String): User? {
+        return try {
+            val response = apiService.getUser(userName)
+            if (response.isSuccessful) {
+                response.body()
+            } else {
+                throw Exception("Error Occurred: ${response.body()}")
+            }
+        } catch (ex: Exception) {
+            Log.e("UserRepo", "Error getting users", ex)
+            throw Exception("Error Occurred: ${ex.message}")
+
+        }
+    }
+
+    suspend fun editUser(userName: String, body: Map<String, String>) {
+        // body would just be a mapOf("password" to "<newPass>) I think
+        try {
+            val response = apiService.editUser(userName, body)
+            if (response.isSuccessful) {
+                Log.w("UserRepo", "Edited User $userName")
+            } else {
+                throw Exception("Error Occurred: ${response.body()}")
+            }
+        } catch (ex: Exception) {
+            Log.e("UserRepo", "Error editing user $userName", ex)
+            throw Exception("Error Occurred: ${ex.message}")
+
+        }
+    }
+
     suspend fun loadUsersToDb(users: List<User>) {
         withContext(Dispatchers.IO) {
             for (user in users) {
@@ -37,14 +68,14 @@ class UserRepository(val userDao: UserDao) {
         }
     }
 
-    suspend fun getUserFromDb(userName: String, password: String) : User {
+    suspend fun getUserFromDb(userName: String, password: String): User {
         return withContext(Dispatchers.IO) {
             val user = userDao.getUser(userName, password)
             user ?: throw NoSuchElementException("No user with $userName and $password")
         }
     }
 
-    suspend fun editUser(userName: String, newName:String?, newPassword:String){
+    suspend fun editUser(userName: String, newName: String?, newPassword: String) {
         withContext(Dispatchers.IO) {
             if (newName != null) {
                 userDao.editUser(userName, newName, newPassword)
@@ -55,7 +86,7 @@ class UserRepository(val userDao: UserDao) {
 
     }
 
-    suspend fun registerNewUser(userName: String, email:String, password:String) {
+    suspend fun registerNewUser(userName: String, email: String, password: String) {
         withContext(Dispatchers.IO) {
             val newUser = User(userName, email, password)
             userDao.insert(newUser)
