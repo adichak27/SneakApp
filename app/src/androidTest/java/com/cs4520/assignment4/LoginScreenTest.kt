46e8c9ebad8af1da7206354feeec4a7a5a85a2a2
patrickyu117 <patrickyu117@gmail.com>
2024-04-16 18:09:19 -0400

    changing screen test not working

--- app/src/androidTest/java/com/cs4520/assignment4/LoginScreenTest.kt
+++ app/src/androidTest/java/com/cs4520/assignment4/LoginScreenTest.kt
@@ -1,13 +1,18 @@
 package com.cs4520.assignment4
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.compose.rememberNavController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.cs4520.sneak.AmazingProductsApp
 import com.cs4520.sneak.CheckoutScreen
 import com.cs4520.sneak.LoginScreen
 import com.cs4520.sneak.MainActivity
@@ -31,8 +36,8 @@ class LoginScreenTest {
             LoginScreen(navController, vm = UserListViewModel(ApplicationProvider.getApplicationContext()))
         }
         // Check for Input Fields
-        composeTestRule.onNodeWithTag("UsernameField").assertExists()
-        composeTestRule.onNodeWithTag("PasswordField").assertExists()
+        composeTestRule.onNodeWithTag("UsernameInput").assertExists()
+        composeTestRule.onNodeWithTag("PasswordInput").assertExists()
 
         // Check for Logo Text
         composeTestRule.onNodeWithTag("LogoText").assertExists()
@@ -41,5 +46,22 @@ class LoginScreenTest {
         composeTestRule.onNodeWithTag("LoginButton").assertExists()
         composeTestRule.onNodeWithTag("ForgotPasswordButton").assertExists()
         composeTestRule.onNodeWithTag("CreateAccountButton").assertExists()
+    }
+
+    @Test
+    fun loginScreenNavigatesToProductScreen() {
+        composeTestRule.setContent {
+            AmazingProductsApp()
+        }
+
+        // Enter username and password
+        composeTestRule.onNodeWithTag("UsernameInput").performTextInput("admin")
+        composeTestRule.onNodeWithTag("PasswordInput").performTextInput("admin")
+
+        // moving to Product screen
+        composeTestRule.onNodeWithTag("LoginButton").performClick()
+
+        // check we are on the Product screen
+        composeTestRule.onNodeWithText("Products").assertIsDisplayed()
     }
 }
